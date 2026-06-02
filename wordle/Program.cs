23bdd365a07439cc16d3efdcd31ddd3c209347e8
Austin Hauger <114182613+ahauger27@users.noreply.github.com>
2026-06-02 22:35:28 +0000

    compare guess to the secret word

--- wordle/Program.cs
+++ wordle/Program.cs
@@ -4,3 +4,25 @@ List<string> words = ["PHASE", "CHAIR", "SHORE", "SNAKE", "MASON"];
 
 string word = words[rand.Next(words.Count)];
 
+int guessesMade = 1;
+
+Console.WriteLine("WURDLE\n");
+
+Console.WriteLine("Guess the 5-letter word.");
+
+Console.WriteLine($"You have 6 guesses.\n");
+
+while (guessesMade <= 6)
+{
+    Console.Write($"GUESS #{guessesMade}: ");
+
+    string guess = Console.ReadLine();
+
+    if (guess.ToUpper() == word)
+    {
+        Console.WriteLine("You win");
+        break;
+    }
+
+    guessesMade += 1;
+}
